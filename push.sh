9a7e2455bf7d7c703cba43eff7e2882e873f9fe6
Richard Chmielek <github@chmielek.com>
2017-10-16 22:20:44 +0100

    updated build

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "server: $server"
 docker login --username $username --password $password $server
 docker tag hexo $server/$username/hexo:latest
 docker push $server/$username/hexo:latest
