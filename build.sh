8eafaa6313430e129797bd5486df858acd8db28c
Richard Chmielek <github@chmielek.com>
2017-10-17 22:29:52 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [ ! -z "$version" ]; then
+    echo "Building hexo: $version..."
     docker build -t docker-hexo\hexo:$version .
+    echo "done."
 fi
+echo "Building hexo: latest..."
 docker build -t docker-hexo\hexo:latest .
+echo "done."
