d22b7df5d703240719de97be24df6e1edb748efa
Richard Chmielek <github@chmielek.com>
2017-10-17 21:59:20 +0100

    added check for version tags

--- push.sh
+++ push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker login --username $username --password $password $server
+if [ ! -z "$version" ]; then
+    echo "Version is set. tagging and pushing new version"
+    docker tag docker-hexo $server/$username/hexo:$version
+    docker push $server/$username/hexo:$version
+fi
+
 docker tag docker-hexo $server/$username/hexo:latest
 docker push $server/$username/hexo:latest
