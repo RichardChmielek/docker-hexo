17393fa56ba53f287e119437d0df9ddbffeebb4f
Richard Chmielek <github@chmielek.com>
2017-10-18 21:45:48 +0100

    updated build

--- build.sh
+++ build.sh
@@ -2,13 +2,8 @@
 if [ ! -z "$version" ]; then
     echo "Building hexo: $version..."
     docker build --tag "docker-hexo/hexo:$version" --no-cache=true .
-    echo "Tagging hexo: $version..."
-    docker tag docker-hexo $server/blog/hexo:$version
-    echo "Pushing hexo: $version..."
     echo "Done."
 fi
 echo "Building hexo: latest..."
 docker build --tag docker-hexo/hexo:latest --no-cache=true .
-echo "Tagging hexo: latest..."
-docker tag docker-hexo $server/blog/hexo:latest
 echo "Done."
--- push.sh
+++ push.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 docker login $server --username $username --password $password
 if [ ! -z "$version" ]; then
+    echo "Tagging hexo: $version..."
+    docker tag hexo $server/blog/hexo:$version
+    echo "Pushing hexo: $version..."
     docker push $server/blog/hexo:$version
-    echo "pushed $version."
+    echo "Pushed $version."
 fi
+echo "Tagging hexo: latest..."
+docker tag hexo $server/blog/hexo:latest
+echo "Pusing latest..."
 docker push $server/blog/hexo:latest
-echo "tagged and pushed latest."
+echo "Tagged and pushed latest."
