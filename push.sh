ad69419af662764ea34748f8e4dc114bf25760ce
Richard Chmielek <github@chmielek.com>
2017-10-20 22:28:49 +0100

    updated push script

--- push.sh
+++ push.sh
@@ -1,10 +1,33 @@
-#!/bin/bash
+#!/bin/sh
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -s|--server=*)
+      server="${1#*=}"
+      ;;
+    -u|--username=*)
+      username="${1#*=}"
+      ;;
+    -c|--container=*)
+      container="${1#*=}"
+      ;;
+    -v|--version=*)
+      version="${1#*=}"
+      ;;
+    *)
+      echo "********************************\n"
+      echo "* Error: Unexpected parameters.*\n"
+      echo "********************************\n"
+      exit 1
+  esac
+  shift
+done
+
 docker login $server --username $username --password $password
 if [ ! -z "$version" ]; then
     echo "Tagging hexo: $version..."
-    docker tag hexo $server/blog/hexo:$version
+    docker tag hexo $server/$container:$version
     echo "Pushing hexo: $version..."
-    docker push $server/blog/hexo:$version
+    docker push $server/$container:$version
     echo "Pushed $version."
 fi
 echo "Tagging hexo: latest..."
